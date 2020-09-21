7c2f1230963c2b3f11c5cd92c34ffdd50bd967f0
Nihal K P <nihal44.kp@gmail.com>
2020-09-21 18:53:33 +0530

    config modified

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost","root","","mobiles_cart");
+    $conn = new mysqli("localhost","root","","mobile_store");
     if($conn->connect_error){               //to check if database connection is error
         die("connection failed!".$conn->connect_error);
     }
