947f2dd837a189043dfa72936e9aa0648c23b10a
Nihal K P <nihal44.kp@gmail.com>
2020-09-24 10:15:45 +0530

    Config.php modified for infinityfree's database

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost","root","","mobile_store");
+    $conn = new mysqli("sql110.epizy.com","epiz_26806643","Zw2IXmCGE7","epiz_26806643_mobstore");
     if($conn->connect_error){               //to check if database connection is error
         die("connection failed!".$conn->connect_error);
     }
